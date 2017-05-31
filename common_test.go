2fa524f9377d49d7cd15cd161db48956fa68eb8c
Santiago M. Mola <santi@mola.io>
2017-05-31 12:57:14 +0200

    fix default image reference for docker transport
    docker: requires // prefix, docker-daemon: requires absence of it.

--- common_test.go
+++ common_test.go
@@ -1,7 +1,20 @@
 package server
 
-import "github.com/Sirupsen/logrus"
+import (
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+)
 
 func init() {
 	logrus.SetLevel(logrus.DebugLevel)
+}
+
+func TestDefaultDriverImageReference(t *testing.T) {
+	require := require.New(t)
+
+	require.Equal("docker://bblfsh/python-driver:latest", DefaultDriverImageReference("docker", "python"))
+	require.Equal("docker://bblfsh/python-driver:latest", DefaultDriverImageReference("", "python"))
+	require.Equal("docker-daemon:bblfsh/python-driver:latest", DefaultDriverImageReference("docker-daemon", "python"))
 }
