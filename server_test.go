c34d0e28d24ec949e31ff6bbd0f6affdde152f4b
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2017-07-10 11:55:17 +0200

    server: Add tests for --max-message-size option

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
@@ -16,6 +17,8 @@ import (
 	"github.com/bblfsh/sdk/uast"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type echoDriver struct{}
@@ -112,4 +115,172 @@ func TestDefaultDriverImageReference(t *testing.T) {
 	s = NewServer(r, python_override)
 	s.Transport = "docker-daemon"
 	require.Equal("overriden", s.defaultDriverImageReference("python"))
+}
+
+func TestMaxMessageSizeExceeded(t *testing.T) {
+	require := require.New(t)
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "bblfsh-runtime")
+	r := runtime.NewRuntime(tmpDir)
+	err = r.Init()
+	require.NoError(err)
+
+	s := NewServer(r, make(map[string]string))
+	dp, err := StartDriverPool(DefaultScalingPolicy(), DefaultPoolTimeout, func() (Driver, error) {
+		return &echoDriver{}, nil
+	})
+	require.NoError(err)
+	require.NotNil(dp)
+
+	s.drivers["python"] = dp
+
+	lis, err := net.Listen("tcp", "localhost:0")
+	require.NoError(err)
+	go (&GRPCServer{s, []grpc.ServerOption{}}).Serve(lis)
+
+	time.Sleep(time.Second * 1)
+
+	conn, err := grpc.Dial(lis.Addr().String(), grpc.WithInsecure(), grpc.WithTimeout(time.Second*2))
+	require.NoError(err)
+	client := protocol.NewProtocolServiceClient(conn)
+	_, err = client.ParseUAST(context.TODO(), &protocol.ParseUASTRequest{Content: bigContent()})
+	require.NotNil(err)
+	status, _ := status.FromError(err)
+	require.Equal(status.Code(), codes.ResourceExhausted)
+	err = conn.Close()
+	require.NoError(err)
+	err = s.Close()
+	require.NoError(err)
+}
+
+func TestMaxMessageSizeExceededInClient(t *testing.T) {
+	require := require.New(t)
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "bblfsh-runtime")
+	r := runtime.NewRuntime(tmpDir)
+	err = r.Init()
+	require.NoError(err)
+
+	s := NewServer(r, make(map[string]string))
+	dp, err := StartDriverPool(DefaultScalingPolicy(), DefaultPoolTimeout, func() (Driver, error) {
+		return &echoDriver{}, nil
+	})
+	require.NoError(err)
+	require.NotNil(dp)
+
+	s.drivers["python"] = dp
+
+	lis, err := net.Listen("tcp", "localhost:0")
+	require.NoError(err)
+	serverOptions := []grpc.ServerOption{
+		grpc.MaxRecvMsgSize(8 * 1024 * 1024),
+		grpc.MaxSendMsgSize(8 * 1024 * 1024)}
+
+	go (&GRPCServer{s, serverOptions}).Serve(lis)
+
+	time.Sleep(time.Second * 1)
+
+	conn, err := grpc.Dial(lis.Addr().String(), grpc.WithInsecure(), grpc.WithTimeout(time.Second*2))
+	require.NoError(err)
+	client := protocol.NewProtocolServiceClient(conn)
+	_, err = client.ParseUAST(context.TODO(), &protocol.ParseUASTRequest{Content: bigContent()})
+	require.NotNil(err)
+	status, _ := status.FromError(err)
+	require.Equal(status.Code(), codes.ResourceExhausted)
+	err = conn.Close()
+	require.NoError(err)
+	err = s.Close()
+	require.NoError(err)
+}
+
+func TestMaxMessageSizeExceededInServer(t *testing.T) {
+	require := require.New(t)
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "bblfsh-runtime")
+	r := runtime.NewRuntime(tmpDir)
+	err = r.Init()
+	require.NoError(err)
+
+	s := NewServer(r, make(map[string]string))
+	dp, err := StartDriverPool(DefaultScalingPolicy(), DefaultPoolTimeout, func() (Driver, error) {
+		return &echoDriver{}, nil
+	})
+	require.NoError(err)
+	require.NotNil(dp)
+
+	s.drivers["python"] = dp
+
+	lis, err := net.Listen("tcp", "localhost:0")
+	require.NoError(err)
+	go (&GRPCServer{s, []grpc.ServerOption{}}).Serve(lis)
+
+	time.Sleep(time.Second * 1)
+
+	callOptions := []grpc.CallOption{
+		grpc.MaxCallRecvMsgSize(8 * 1024 * 1024),
+		grpc.MaxCallSendMsgSize(8 * 1024 * 1024)}
+	conn, err := grpc.Dial(
+		lis.Addr().String(),
+		grpc.WithInsecure(),
+		grpc.WithTimeout(time.Second*2),
+		grpc.WithDefaultCallOptions(callOptions...))
+	require.NoError(err)
+	client := protocol.NewProtocolServiceClient(conn)
+	_, err = client.ParseUAST(context.TODO(), &protocol.ParseUASTRequest{Content: bigContent()})
+	require.NotNil(err)
+	status, _ := status.FromError(err)
+	require.Equal(status.Code(), codes.ResourceExhausted)
+	err = conn.Close()
+	require.NoError(err)
+	err = s.Close()
+	require.NoError(err)
+}
+
+func TestMaxMessageSizeNotExceeded(t *testing.T) {
+	require := require.New(t)
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "bblfsh-runtime")
+	r := runtime.NewRuntime(tmpDir)
+	err = r.Init()
+	require.NoError(err)
+
+	s := NewServer(r, make(map[string]string))
+	dp, err := StartDriverPool(DefaultScalingPolicy(), DefaultPoolTimeout, func() (Driver, error) {
+		return &echoDriver{}, nil
+	})
+	require.NoError(err)
+	require.NotNil(dp)
+
+	s.drivers["python"] = dp
+
+	lis, err := net.Listen("tcp", "localhost:0")
+	require.NoError(err)
+	serverOptions := []grpc.ServerOption{
+		grpc.MaxRecvMsgSize(8 * 1024 * 1024),
+		grpc.MaxSendMsgSize(8 * 1024 * 1024)}
+	go (&GRPCServer{s, serverOptions}).Serve(lis)
+
+	time.Sleep(time.Second * 1)
+
+	callOptions := []grpc.CallOption{
+		grpc.MaxCallRecvMsgSize(8 * 1024 * 1024),
+		grpc.MaxCallSendMsgSize(8 * 1024 * 1024)}
+	conn, err := grpc.Dial(
+		lis.Addr().String(),
+		grpc.WithInsecure(),
+		grpc.WithTimeout(time.Second*2),
+		grpc.WithDefaultCallOptions(callOptions...))
+	require.NoError(err)
+	client := protocol.NewProtocolServiceClient(conn)
+	_, err = client.ParseUAST(context.TODO(), &protocol.ParseUASTRequest{Content: bigContent()})
+	require.NoError(err)
+	err = conn.Close()
+	require.NoError(err)
+	err = s.Close()
+	require.NoError(err)
+}
+
+// Generate a string longer than 4MB, which is the default message limit in GRPC
+func bigContent() string {
+	content := bytes.NewBufferString("")
+	for i := 0; i < 65536; i++ {
+		content.WriteString("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef")
+	}
+	return content.String()
 }
