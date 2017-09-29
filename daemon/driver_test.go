f4eac36149aae285358c8d144098caba0c5ec9c3
Máximo Cuadros <mcuadros@gmail.com>
2017-09-29 02:16:13 +0200

    daemon: DriverIntance test, use a experimental grpc image

--- daemon/driver_test.go
+++ daemon/driver_test.go
@@ -15,7 +15,16 @@ func init() {
 	runtime.Bootstrap()
 }
 
+func IfNetworking(t *testing.T) {
+	if len(os.Getenv("TEST_NETWORKING")) != 0 {
+		return
+	}
+
+	t.Skip("skipping network test use TEST_NETWORKING to run this test")
+}
 func TestNewDriver(t *testing.T) {
+	IfNetworking(t)
+
 	r := require.New(t)
 
 	dir, err := ioutil.TempDir(os.TempDir(), "bblfsh-runtime")
@@ -29,7 +38,7 @@ func TestNewDriver(t *testing.T) {
 	err = run.Init()
 	r.Nil(err)
 
-	image, err := runtime.NewDriverImage("docker-daemon:bblfsh/python-driver:dev-0b6ddb4-dirty")
+	image, err := runtime.NewDriverImage("docker://bblfsh/python-driver:experimental")
 	r.Nil(err)
 
 	err = run.InstallDriver(image, false)
