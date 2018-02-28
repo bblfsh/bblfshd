33be7b091fdd6db8789c681ddffa5a68da9d70a7
Juanjo Alvarez <juanjo@sourced.tech>
2018-02-28 11:33:05 +0100

    Added SELinux section to the readme
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- selinux/compile.sh
+++ selinux/compile.sh
@@ -2,6 +2,4 @@
 
 checkmodule -M -m -o bblfshd.mod bblfshd.te && \
     semodule_package -o bblfshd.pp -m bblfshd.mod && \
-    echo 'Module compiled, load it with semodule -i bblfshd.pp'
-
-# enable with: semodule -i bblfshd.pp
+    echo 'Module compiled, load it with "semodule -i bblfshd.pp"'
